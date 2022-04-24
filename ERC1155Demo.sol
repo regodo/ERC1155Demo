82857c5438a9234f91a0f0d8145a0c601cd33611
regodo <59122258+regodo@users.noreply.github.com>
2022-04-24 21:46:06 +0200

    Update ERC1155Demo.sol

--- ERC1155Demo.sol
+++ ERC1155Demo.sol
@@ -7,38 +7,73 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
- * @title ERC1155Demo v1
+ * @title ERC1155Demo v2
  * @notice simple ERC 1155 nft collection
  */
-
 contract ERC1155Demo is ERC1155Supply, Ownable {
-    uint256 public constant BRONZE = 1;
-    uint256 public constant SILVER = 2;
-    uint256 public constant GOLD = 3;
-    string private metaBaseURL = "https://ipfs.io/ipfs/QmNzUReme8udrveFWppQpkmPD7yeyD2gs2AGeXPRkZwnbH/";
-    string private metaFileExtension = ".json";
+    mapping(uint256 => uint256) public maxTotalSupply;
+    mapping(uint256 => uint256) public mintPrice;
+    mapping(uint256 => string) public metaDataURL;
+    mapping(uint256 => bool) public saleStatus;
+    uint256 public highestTokenID = 0;
 
     constructor() ERC1155("") {
-        _mint(msg.sender, BRONZE, 1000, "");
-        _mint(msg.sender, SILVER, 100, "");
-        _mint(msg.sender, GOLD, 10, "");
+        maxTotalSupply[0] = 2000;
+        mintPrice[0] = 0.0001 ether;
+        saleStatus[0] = true;
+        metaDataURL[0] = "https://ipfs.io/ipfs/Qmb4E8BRdKVxZYhoZ5LRcd7vZYfxmyff9ZipuLqM9pdP1k/0.json";
+        _mint(msg.sender, 0, 1, "");
+    }
+
+    function Buy(uint256 _tokenid, uint256 _amount) public payable {
+        require(msg.sender == tx.origin, "No transaction from smart contracts!");
+        require(tokenExists(_tokenid), "This Token is not awailable!");
+        require(saleStatus[_tokenid], "Sale must be active to mint!");
+        require(totalSupply(_tokenid) + _amount <= maxTotalSupply[_tokenid], "Purchase would exceed max supply!");
+        require(msg.value >= mintPrice[_tokenid] * _amount, "Not enough ETH for transaction!");
+
+        _mint(msg.sender, _tokenid, _amount, "");
+    }
+
+    function AirDrop(uint256 _tokenid, uint256 _amount, address _recepient) public onlyOwner {
+        require(_tokenid <= highestTokenID, "This Token is not awailable!");
+        require(totalSupply(_tokenid) + _amount <= maxTotalSupply[_tokenid], "Purchase would exceed max supply!");
+        _mint(_recepient, _tokenid, _amount, "");
     }
 
     function uri(uint256 _tokenid) override public view returns (string memory) {
-        return string(
-            abi.encodePacked(
-                metaBaseURL,
-                Strings.toString(_tokenid),
-                metaFileExtension
-            )
-        );
+        return metaDataURL[_tokenid];
     }
 
-    function chnageMetaBaseURL(string memory _newurl) public onlyOwner {
-        metaBaseURL = _newurl;
+    function tokenExists(uint256 _tokenid) public view returns (bool) {
+        return _tokenid <= highestTokenID;
     }
 
-    function chnageMetaFileExtension(string memory _newext) public onlyOwner {
-        metaFileExtension = _newext;
+    function chnageMaxTotalSupply(uint256 _tokenid, uint256 _newMaxTotalSupply) public onlyOwner {
+        maxTotalSupply[_tokenid] = _newMaxTotalSupply;
+    }
+
+    function chnageMintPrice(uint256 _tokenid, uint256 _newMintPrice) public onlyOwner {
+        mintPrice[_tokenid] = _newMintPrice;
+    }
+
+    function flipSaleState(uint256 _tokenid) public onlyOwner {
+        saleStatus[_tokenid] = !saleStatus[_tokenid];
+    }
+
+    function chnageMetaDataURL(uint256 _tokenid, string memory _newurl) public onlyOwner {
+        metaDataURL[_tokenid] = _newurl;
+    }
+
+    function addNewToken(uint256 _maxTotalSupply, uint256 _mintPrice, bool _saleStatus, string memory _metaDataURL) public onlyOwner {
+        highestTokenID++;
+        maxTotalSupply[highestTokenID] = _maxTotalSupply;
+        mintPrice[highestTokenID] = _mintPrice;
+        saleStatus[highestTokenID] = _saleStatus;
+        metaDataURL[highestTokenID] = _metaDataURL;
+    }
+
+    function Withdraw() public onlyOwner {
+        Address.sendValue(payable(owner()), address(this).balance);
     }
 }
